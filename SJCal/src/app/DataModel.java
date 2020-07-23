dcb6f1f88a42b53f7f81d63da9b6194f83e23b0a
Christian Santos <cesantos647@gmail.com>
2020-07-22 17:25:36 -0700

    implement day view

--- SJCal/src/app/DataModel.java
+++ SJCal/src/app/DataModel.java
@@ -1,7 +1,9 @@
 package app;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class DataModel {
 	private ArrayList<Event> oneTimeEvents;
@@ -13,20 +15,89 @@ public class DataModel {
 	
 	
 	public DataModel() {
+		oneTimeEvents = new ArrayList<>();
+		recurringEvents = new ArrayList<>();
 		today = LocalDate.now();
 		reference = LocalDate.now();
 		currentCal = LocalDate.now();
+		style = ViewStyle.DAY;
+		
+		Event test = new Event("test", LocalTime.of(12, 0), LocalTime.of(15, 0), LocalDate.now());
+		Event test2 = new Event("test2", LocalTime.of(10, 0), LocalTime.of(11, 0), LocalDate.now());
+		Event test3 = new Event("test3", LocalTime.of(16, 0), LocalTime.of(18, 0), LocalDate.now());
+		oneTimeEvents.add(test);
+		oneTimeEvents.add(test2);
+		oneTimeEvents.add(test3);
 	}
 	
 	public void setViewStyle(ViewStyle newStyle) {
-		//
+		style = newStyle;
 	}
 	
-	public String getEvents(LocalDate date) {		
+	public LocalDate getReference() {
+		return reference;
+	}
+	
+	public LocalDate getToday() {
+		return today;
+	}
+	
+	public LocalDate getCalDate() {
+		return currentCal;
+	}
+	
+	public void setReference(LocalDate date) {
+		reference = date;
+	}
+	
+	public void setCalDate(LocalDate date) {
+		currentCal = date;
+	}
+	
+	public String getEvents(LocalDate date) {
+		if(style == ViewStyle.DAY) {
+			return getDayEvents(date);
+		}
+		else if(style == ViewStyle.WEEK) {
+			return getWeekEvents(date);
+		}
+		else if(style == ViewStyle.MONTH) {
+			return getMonthEvents(date);
+		}
+		else {
+			return "An error has occurred";
+		}
+
+	}
+	
+	public String getAgendaEvents(LocalDate startDate, LocalDate endDate) {
 		return "";
 	}
 	
-	public String getEvents(LocalDate startDate, LocalDate endDate) {
+	public String getDayEvents(LocalDate date) {
+		TreeSet<Event> events = new TreeSet<>();
+		for(Event e : oneTimeEvents) {
+			if(e.getDate().isEqual(date)) {
+				events.add(e);
+			}
+		}
+		for(RecurringEvent re : recurringEvents) {
+			if(re.getDaysOfWeek().contains(date.getDayOfWeek()) && re.isWithin(date)) {
+				events.add(new Event(date, re));
+			}
+		}
+		String eventString = date.format(Event.FORMALDATEFORMATTER) + "\n";
+		for(Event e : events) {
+			eventString = eventString + e.toString();
+		}
+		return eventString;
+	}
+	
+	public String getWeekEvents(LocalDate date) {
+		return "";
+	}
+	
+	public String getMonthEvents(LocalDate date) {
 		return "";
 	}
 	
--- SJCal/src/view/View.java
+++ SJCal/src/view/View.java
@@ -1,12 +1,17 @@
 package view;
 
 import javax.swing.*;
+
+import app.DataModel;
+import app.ViewStyle;
+
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class View {
     public static void main(String[] args) {
+    	DataModel data = new DataModel();
         JFrame frame = new JFrame();
 
         JPanel appViewPanel = new JPanel();
@@ -20,24 +25,24 @@ public class View {
         JScrollPane scrollPane = new JScrollPane( area );
 
         mainViewPanel.add(new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 3)){{
-            add(createButtonWithEventListener(area, "Day Button Clicked!", "Day"));
-            add(createButtonWithEventListener(area, "Week Button Clicked!", "Week"));
-            add(createButtonWithEventListener(area, "Month Button Clicked!", "Month"));
-            add(createButtonWithEventListener(area, "Agenda Button Clicked!", "Agenda"));
+            add(createButtonWithEventListener(area, "Day", ViewStyle.DAY, data));
+            add(createButtonWithEventListener(area, "Week", ViewStyle.WEEK, data));
+            add(createButtonWithEventListener(area, "Month", ViewStyle.MONTH, data));
+            add(createButtonWithEventListener(area, "Agenda", ViewStyle.MONTH, data));
         }});
         mainViewPanel.add(scrollPane, BorderLayout.PAGE_START);
 
         leftViewPanel.setLayout(new BoxLayout(leftViewPanel, BoxLayout.Y_AXIS));
         leftViewPanel.add(new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 3)){{
-            add(createButtonWithEventListener(area, "Today Button Clicked!", "Today"));
-            add(createButtonWithEventListener(area, "< Button Clicked!", "<"));
-            add(createButtonWithEventListener(area, "> Button Clicked!", ">"));
+            //add(createButtonWithEventListener(area, "Today Button Clicked!", "Today", ));
+            //add(createButtonWithEventListener(area, "< Button Clicked!", "<"));
+            //add(createButtonWithEventListener(area, "> Button Clicked!", ">"));
         }});
-        leftViewPanel.add(createButtonWithEventListener(area, "CREATE Button Clicked!", "CREATE"));
+        //leftViewPanel.add(createButtonWithEventListener(area, "CREATE Button Clicked!", "CREATE"));
 
         rightViewPanel.setLayout(new BoxLayout(rightViewPanel, BoxLayout.Y_AXIS));
         rightViewPanel.add(new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 3)){{
-            add(createButtonWithEventListener(area, "From File Button Clicked!", "From File"));
+            //add(createButtonWithEventListener(area, "From File Button Clicked!", "From File"));
         }});
 
         appViewPanel.setLayout(new FlowLayout());
@@ -51,12 +56,14 @@ public class View {
         frame.setVisible(true);
     }
 
-    public static JButton createButtonWithEventListener(JTextArea componentToUpdate,
-                                                        String valueToUpdate, String labelName){
+    public static JButton createButtonWithEventListener(JTextArea componentToUpdate, String labelName,
+                                                        ViewStyle style, DataModel data){
         return new JButton(labelName){{
             addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
+                	data.setViewStyle(style);
+                	String valueToUpdate = data.getEvents(data.getReference());
                     componentToUpdate.setText(valueToUpdate);
                 }
             });
